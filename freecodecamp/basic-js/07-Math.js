ff0d246e51faef49517c3ffd399eb3f67712e5fc
aysedemirel <aysedemirel1996@gmail.com>
2021-09-11 22:33:19 +0300

    [NEW] Add Math example

---
+++ freecodecamp/basic-js/07-Math.js
@@ -0,0 +1,11 @@
+function randomWholeNum() {
+  return Math.floor(Math.random() * 10 );
+}
+randomWholeNum();
+/////////////////////////////
+function randomRange(myMin, myMax) {
+  return Math.floor(Math.random() *(myMax-myMin+1))+myMin;
+}
+randomRange(3,100);
+/////////////////////////////
+
