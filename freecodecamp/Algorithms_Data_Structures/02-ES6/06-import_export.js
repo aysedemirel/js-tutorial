ef87faad78e33bb500c3b70c63e76d766e95342b
aysedemirel <aysedemirel1996@gmail.com>
2021-10-28 21:36:10 +0300

    [NEW] Add import-export examples

---
+++ freecodecamp/Algorithms_Data_Structures/02-ES6/06-import_export.js
@@ -0,0 +1,28 @@
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
+// export{uppercaseString,lowercaseString};
+
+/////////////////////////////////
+//import {uppercaseString,lowercaseString}  from './string_functions.js';
+//uppercaseString("hello");
+//lowercaseString("WORLD!");
+
+/////////////////////////////////
+// import *  as stringFunctions from './string_functions.js';
+// stringFunctions.uppercaseString("hello");
+// stringFunctions.lowercaseString("WORLD!");
+
+/////////////////////////////////
+// export default function add(x, y) {
+//   return x + y;
+// }
+
+/////////////////////////////////
+// import subtract from './math_functions.js';  
+// subtract(7,4);
+
+/////////////////////////////////
